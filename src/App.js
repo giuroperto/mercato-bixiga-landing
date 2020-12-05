7baaaf2f47aeca76910580cafe72835d755540da
Giu Roperto <giu.roperto@gmail.com>
2020-12-05 00:28:52 -0300

    starting structure of the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { GrInstagram } from "react-icons/gr";
+import { RiWhatsappLine } from "react-icons/ri";
 
-function App() {
+import './App.css';
+import BottomFooter from './Components/features/BottomFooter/BottomFooter';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mercato Bixiga</h1>
       </header>
+      <div className="infos">
+        <div className="sqre-background">
+          <p>Em breve, novidades por aqui. Enquanto isso, nos acompanhe em nosso Instagram, ou entre em contato pelo WhatsApp.</p>
+          <div className="contact-info">
+            <div className="insta-info">
+              <GrInstagram />
+              <a href="https://www.instagram.com/mercatobixiga/" target="_blank" className="links">
+                <span>mercatobixiga</span>
+              </a>
+            </div>
+            <div className="whatsapp-info">
+              <RiWhatsappLine />
+              <a href="" className="links">
+                <span>(11)xxxx-xxxx</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <BottomFooter />
     </div>
   );
 }
---
+++ src/Components/features/BottomFooter/BottomFooter.js
@@ -0,0 +1,12 @@
+import './BottomFooter.css';
+import { MdFavorite } from "react-icons/md";
+
+const BottomFooter = () => {
+  return (
+    <div className="bottom-footer">
+      <p>Criado com <MdFavorite /> por e-Roperto</p>
+    </div>
+  );
+}
+
+export default BottomFooter;
